947ddd4c5396fd9c72da7187021b66b7ff3b0e1e
CHIN\a5566 <a5566qq2581@gmail.com>
2024-05-02 15:56:12 +0800

    Published branch "dev"

--- dist/types/libs/hl7/component.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Delimiters } from "./delimiters";
-export declare class Hl7Component {
-    value: (string | string[])[];
-    constructor(...args: (string | string[])[]);
-    toString(delimiters: Delimiters): string;
-}
-//# sourceMappingURL=component.d.ts.map
--- dist/types/libs/hl7/field.d.ts
+++
@@ -1,9 +0,0 @@
-import { Hl7Component } from "./component";
-import { Delimiters } from "./delimiters";
-export declare class Hl7Field {
-    #private;
-    value: (Hl7Component | Hl7Component[])[];
-    constructor(...args: (string | string[])[] | ((string | string[])[])[]);
-    toString(delimiters: Delimiters): string;
-}
-//# sourceMappingURL=field.d.ts.map
